507d3b4da02813106a47d876652f9d8fda4a997c
Kyrylo Storozhenko <busterakel@gmail.com>
2024-05-02 12:37:43 +0300

    minor: created confiig.py with technologies

---
+++ config.py
@@ -0,0 +1,44 @@
+import re
+
+TECHNOLOGIES = [
+    "django",
+    "flask",
+    "pandas",
+    "numpy",
+    "scipy",
+    "drf",
+    "python",
+    "postgres",
+    "postgresql"
+    "redis",
+    "aws",
+    "docker",
+    "rest",
+    "celery",
+    "fastapi",
+    "nginx",
+    "numpy",
+    "pandas",
+    "matplotlib",
+    "sqlite",
+    "beautifulsoup",
+    "pytest",
+    "unittest",
+    "docker",
+    "html",
+    "css",
+    "javascript",
+    "asyncio",
+    "async",
+    "bash",
+    "git",
+    "multiprocessing",
+    "selenium",
+    "threading",
+    "pydantic",
+    "flask-sqlalchemy",
+    "sqlalchemy",
+    "swagger"
+]
+
+
